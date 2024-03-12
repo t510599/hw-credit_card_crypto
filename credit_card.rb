5682cfbe021a21d28d0052534ee0f919469580c0
Tony Yang <t510599@gmail.com>
2024-03-12 16:49:53 +0800

    feat: CreditCard data class

--- credit_card.rb
+++ credit_card.rb
@@ -8,13 +8,21 @@ class CreditCard
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above
+    # initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      # setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
@@ -25,21 +33,26 @@ class CreditCard
 
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
-    # TODO: deserializing a CreditCard object
+    # deserializing a CreditCard object
+    card = JSON.parse(card_s)
+    CreditCard.new(card['number'], card['expiration_date'], card['owner'], card['credit_network'])
   end
 
   # return a hash of the serialized credit card object
   def hash
-    # TODO: implement this method
+    # implement this method
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    to_s.hash
   end
 
   # return a cryptographically secure hash
   def hash_secure
-    # TODO: implement this method
+    # implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    require 'digest'
+    Digest::SHA256.hexdigest(to_s)
   end
 end
